d77fb171f3bc0d2506f07df0d3ef0f2c2fdf2d19
ravenclown <58143354+ravenclown@users.noreply.github.com>
2021-08-15 00:18:42 +0300

    trims fastq's using fastp

--- fastp_bash.sh
+++ fastp_bash.sh
@@ -1,6 +1,16 @@
-#!/usr/bin
-for i in $(seq 1 45);
-do
-  cd /home/mert/Masaüstü/fusarium/$i/clean_data/
-  fastp -z --detect_adapter_for_pe --in1 *_1.fq.gz --out1 ${i}_read1 --in2 *_2.fq.gz --out2 ${i}_read2
-done;
+#!/usr/bin/env bash
+for x in $(seq 1 45); do
+  if [ -d "/home/mert/Desktop/fusarium/$x" ]
+  then
+    cd /home/mert/Desktop/fusarium/$x/clean_data/
+    declare -a arr
+    i=0
+    for file in *.fq.gz; do
+      arr[$i]=$file
+      let i++;
+    done
+    out1=${x}_read1.fq.gz
+    out2=${x}_read2.fq.gz
+    fastp --in1 ${arr[0]} --out1 ${out1} --in2 ${arr[1]} --out2 -z ${out2};
+  fi
+done
