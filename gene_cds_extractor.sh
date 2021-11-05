9b9fa617e4029541fbaeb9d3cf970618af3a83aa
Mert <58143354+ravenclown@users.noreply.github.com>
2021-11-05 16:01:05 +0300

    Snap snipp does not work , needs to be fixed

--- gene_cds_extractor.sh
+++ gene_cds_extractor.sh
@@ -6,17 +6,19 @@ snap_loc=~/Desktop/Snap/SNAP.pl
 mkdir gene_files
 mkdir align_files
 mkdir snap_outputs
+mkdir tab_files
 
 #main loop per gene
-for i in $(seq -w 17781);do
+for i in 00001 00002 00003;do
 
 #create temp folder
 mkdir temp_files
 
 #2nd loop per file
 for j in *.fasta; do
-sample=echo cut -d "_" -f 2 ${j}
-grep "CDS:FOXG_${i}" ${j} | cut -d '>' -f 2 | seqtk subseq ${j} - | union -filter | sed "s/>.*/>${i}_/" > ./temp_files/${j}_${sample}.fasta
+name="${j#temp_}";
+name="${name%_*_*}";
+grep "CDS:FOXG_${i}" ${j} | cut -d '>' -f 2 | seqtk subseq ${j} - | union -filter | sed "s/>.*/>${i}_${name}/" > ./temp_files/${j}_${i}.fasta
 done
 
 #concat CDS sequences in one file
@@ -29,18 +31,19 @@ rm -r temp_files/
 mafft ./gene_files/FOXG_${i}.fasta > ./align_files/FOXG_${i}.fasta.align
 
 #Change align file to TAB delim file using seqkit
-seqkit fx2tab ./align_files/FOXG_${i}.fasta.align > ./tab_files/FOXG_${i}.tab.align 
+seqkit fx2tab ./align_files/FOXG_${i}.fasta.align > ./tab_files/FOXG_${i}.tab.align
 
-#SNAP.pl only accepts uppercase bases and N , - | change chars 
+#SNAP.pl only accepts uppercase bases and N , - | change chars
 cat ./tab_files/FOXG_${i}.tab.align | tr [:lower:] [:upper:] > ./tab_files/temp.tab
 cat ./tab_files/temp.tab > ./tab_files/FOXG_${i}.tab.align
 rm ./tab_files/temp.tab
 
-#create new folder for each gene snap, because snap creates several files 
+#TODO DOES NOT WORK, SNAP HAS NO OUTPUT COMMAND
+#create new folder for each gene snap, because snap creates several files
 cd snap_outputs
 mkdir FOXG_${i}
 cd FOXG_${i}
-perl $snap_loc FOXG_${i}.tab.align 
+perl $snap_loc FOXG_${i}.tab.align
 cd ../..
 
 done
