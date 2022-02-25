02c4ef5e0a1780052e297423e296dbe12f4425b6
Mert <58143354+ravenclown@users.noreply.github.com>
2022-02-25 10:35:03 +0300

    Update py_cds.py

--- py_cds.py
+++ py_cds.py
@@ -3,6 +3,8 @@ from Bio import SeqIO
 import sys
 import csv
 import re
+import os
+
 
 #functions
 def grep(file_to_search,query_term):
@@ -16,14 +18,15 @@ def grep(file_to_search,query_term):
 def cut_region(fasta,bed,gene):
     gene_loc=grep(bed,gene)
     gene_loc_tab=list(csv.reader(gene_loc, delimiter='\t'))
-    record_dict = SeqIO.to_dict(SeqIO.parse("example.fasta", "fasta"))
+    result=""
     #print(gene_loc_tab)
     for i in range (0,len(gene_loc_tab)):
         chr=gene_loc_tab[i][0]
         start=int(gene_loc_tab[i][1])
         end=int(gene_loc_tab[i][2])
         #print(chr,":",start,":",end)
-        print("%s" %(fasta[chr].seq[start:(end+1)]),end="")
+        result=result+fasta[chr].seq[start:(end+1)]
+    return(result)
 
 #terminal arguments
 fasta_list_input=sys.argv[1]
@@ -34,20 +37,26 @@ gene_list_input=sys.argv[3]
 with open(fasta_list_input) as file:
     fasta_list = file.read().splitlines()
 file.close()
+print(fasta_list)
 with open(bed_list_input) as file:
     bed_list = file.read().splitlines()
 file.close()
+print(bed_list)
 with open(gene_list_input) as file:
     genes = file.read().splitlines()
 file.close()
+print(genes)
 
 #main
 for i in range(0,len(genes)):
-    filename = str(i) + ".fasta"
+    filename = genes[i] + ".fasta"
     file = open (filename,"a")
     for j in range(0,len(fasta_list)) :
-        fasta_record = SeqIO.to_dict(SeqIO.parse(fasta_list[j], "fasta"))
+        with open(str(os.path.normpath(fasta_list[j]))) as handle:
+            fasta_record = SeqIO.to_dict(SeqIO.parse(handle, "fasta"))
         bed_file=bed_list[j]
         cut_region(fasta_record,bed_list[j],genes[i])
         f.write(">",j,"_",genes[i])
-        #f.write(cut_region(fasta_record,bed_file,genes[i]))
+        f.write(cut_region(fasta_record,bed_file,genes[i]))
+        #print(">",(j+1),"_",genes[i])
+        #print(cut_region(fasta_record,bed_file,genes[i]))
