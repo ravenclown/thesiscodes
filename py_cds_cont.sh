4a346ecc6568e0f7e7e21e506fc5a297e3e70489
Mert <58143354+ravenclown@users.noreply.github.com>
2022-03-01 11:16:11 +0300

    Update py_cds_cont.sh

--- py_cds_cont.sh
+++ py_cds_cont.sh
@@ -6,8 +6,8 @@ for i in *.align; do
 seqkit split ${i} -i
 done
 
-for j in 1 3 4 5 7 8 9 11 12 14 34 35 36 37 38; do
-find . -name "*${j}*.align" -exec cat {} + | union -filter  > ${j}.union.fasta
+for i in {1..15}; do 
+find . -name "*id_${i}_*"-exec cat {} + | union -filter  > ${i}.union.fasta 
 done
 
 cat *.union.fasta
