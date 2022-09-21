fc7c4dd1ff9567213b0439b27708a335ca983f37
Mert Yıldız <58143354+ravenclown@users.noreply.github.com>
2022-09-21 09:20:14 +0300

    Create dbcan2_cazy.sh

---
+++ dbcan2_cazy.sh
@@ -0,0 +1,15 @@
+#! usr/bin/bash
+#hmmscan must be installed and added to PATH
+
+HMMPARSER=/path/to/hmmscan-parser.h
+WD=/path/to/files
+FASTA=$1
+OUTFILE=$2
+
+#wget https://bcb.unl.edu/dbCAN2/download/Databases/V11/ -r --no-clobber --no-parent
+#download hmmDB and dbcan2 for cazy
+
+hmmpress dbCAN-HMMdb-V11.txt
+hmmscan --domtblout ${OUTFILE}.out.dm dbCAN-fam-HMMs.txt ${FASTA} > ${OUTFILE}.out
+sh ${HMMPARSER}/hmmscan-parser.sh ${OUTFILE}.out.dm > ${OUTFILE}.out.dm.ps
+cat ${OUTFILE}.out.dm.ps | awk '$5<1e-15&&$10>0.35' > ${OUTFILE}.out.dm.ps.stringent
