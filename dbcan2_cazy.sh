28d040cca7e4e8fb925ccc2f50a7ec6f7e41a82b
Mert Yıldız <58143354+ravenclown@users.noreply.github.com>
2022-09-21 09:21:19 +0300

    Update dbcan2_cazy.sh

--- dbcan2_cazy.sh
+++ dbcan2_cazy.sh
@@ -10,6 +10,6 @@ OUTFILE=$2
 #download hmmDB and dbcan2 for cazy
 
 hmmpress dbCAN-HMMdb-V11.txt
-hmmscan --domtblout ${OUTFILE}.out.dm dbCAN-fam-HMMs.txt ${FASTA} > ${OUTFILE}.out
+hmmscan --domtblout ${OUTFILE}.out.dm dbCAN-HMMdb-V11.txt ${FASTA} > ${OUTFILE}.out
 sh ${HMMPARSER}/hmmscan-parser.sh ${OUTFILE}.out.dm > ${OUTFILE}.out.dm.ps
 cat ${OUTFILE}.out.dm.ps | awk '$5<1e-15&&$10>0.35' > ${OUTFILE}.out.dm.ps.stringent
